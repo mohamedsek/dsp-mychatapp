da4982d508f9848dd6d8853d4ee064db3677120a
mohamedsek <mohamed.sekkoume@gmail.com>
2023-01-13 15:44:17 +0100

    check point login

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken')
 exports.login = async (req, res, next) => {
     try {
         let DataUser = req.body;
-        console.log(DataUser)
+        console.log('this data received from req to Login controller ' + DataUser)
         let User = await UserModel.findOne({ email: DataUser.email });
         console.log(User)
         console.log('>>>>>>>>>>>this the request data : ' + DataUser.hashpassword)
@@ -13,10 +13,10 @@ exports.login = async (req, res, next) => {
         console.log('>>>>>>>>>>>>>>>this is the DB data' + User.hashpassword)
 
                 token = jwt.sign({ email: User.email }, process.env.PRIVATE_KEY, { expiresIn: 60 * 60 })
-                res.json(token);
+                res.status(200).json({message: token, error: false});
             
         }else{
-            res.json({message: 'user not found'})
+            res.status(401).json({message: 'user not found', error: true})
         }
     } catch (err) {
         next(err)
--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,1 +1,63 @@
 console.log("hello world")
+
+
+
+let form = document.getElementById('form')
+let email = document.getElementById('email')
+let hashpassword = document.getElementById('hashpassword')
+
+
+
+
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    data = []
+    var emailKey = encodeURIComponent(email);
+    var emailValue = encodeURIComponent(email.value);
+    data.push(emailKey + "=" + emailValue);
+    var hashpasswordKey = encodeURIComponent(hashpassword);
+    var hashpasswordValue = encodeURIComponent(hashpassword.value);
+    data.push(hashpasswordKey + "=" + hashpasswordValue);
+    //const formData = new FormData(document.querySelector('form'));
+    console.log(data)
+    fetch('/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        },
+        body: data
+    })
+
+    /* console.log(">>>>>>>>>> we're past default behaviour")
+    if (email.value && hashpassword.value) {
+        console.log('>>>>>>>>>> ' + email.value + hashpassword.value)
+        myHeaders = new Headers({
+            "Content-Type": "text/plain"
+        });
+        var myInit = {
+            method: 'POST',
+            headers: myHeaders,
+            mode: 'cors',
+            cache: 'default',
+            body: { email: email.value, hashpassword: hashpassword.value }
+        };
+        console.log(myInit.body)
+        fetch('/login', myInit).then(function (response) {
+            if (!response.error) {
+                // sauvegarde
+                console.log(response)
+                window.localStorage.setItem('jwt', response.message)
+            }
+        })
+    } */
+}
+)
+
+function parseData(data) {
+    if (!data) return {};
+    if (typeof data === 'object') return data;
+    if (typeof data === 'string') return JSON.parse(data);
+
+    return {};
+}
